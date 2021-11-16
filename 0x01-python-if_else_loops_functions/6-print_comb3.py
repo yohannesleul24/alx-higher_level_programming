b6af185c6df1d3b573ac5cbe8d1f0e4bba23cd84
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-11-16 11:45:09 -0800

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-        for j in range(i + 1, 10):
-                    print("{:d}{:d}".format(i, j), end=', ')
-                    print("{:d}{:d}".format(i + 1, j))
+for digit in range(0, 10):
+            for digit1 in range(digit + 1, 10):
+                            if digit == 8 and digit1 == 9:
+                                                print("{}{}".format(digit, digit1))
+                            else:
+                                                print("{}{}".format(digit, digit1), end=", ")
