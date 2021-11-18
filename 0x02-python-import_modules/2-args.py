1b468ec0f0102e2bf620619867bae8e20280e547
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-11-18 06:29:04 -0800

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        import sys
+            n = len(sys.argv) - 1
+                string = "arguments" if n != 1 else "argument"
+                    sign = ":" if n > 0 else "."
+                        print("{} {}{}".format(n, string, sign))
+                            for i in range(1, len(sys.argv)):
+                                        print("{}: {}".format(i, str(sys.argv[i])))
