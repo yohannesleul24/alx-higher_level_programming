c5e6ef112a62c1e205b38e0e0def682709716999
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-13 10:37:20 -0800

    real defn of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Real definition of a rectangle
+
+"""
+
+
+class Rectangle:
+    """class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """The __init__ method of the square class
+        Args:
+            width(int): Private attribute default 0
+            height(int): Private attribute default 0
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Private instance attribute getter method."""
+        return self.__width
+
+    @property
+    def height(self):
+        """Private instance attribute getter method."""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Private instance attribute property setter."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Private instance attribute property setter."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
