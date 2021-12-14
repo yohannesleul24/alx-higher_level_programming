5529c6b06dbd93371b22c4e8595b1aefac11c180
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-14 04:45:55 -0800

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(n=0):
     magic_string.n = getattr(magic_string, "n", 0) + 1
-    return ("Bestschool, " * (magic_string.n - 1) + "Bestschool")
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
