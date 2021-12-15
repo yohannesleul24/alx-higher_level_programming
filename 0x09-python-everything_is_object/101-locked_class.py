190154d83ad6a1443511967db565604bdc2ece23
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-14 23:48:26 -0800

    low memeory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""locked class"""
+
+
 class LockedClass:
-    __slots__ = ('first_name')
-    def __init__(self):
-        pass
+    """locked class"""
+
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=''):
+        self.first_name = first_name
