00119303eef1bda52c5982c41da82f3bd6c97c68
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-20 11:28:02 -0800

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
 """
-inherits from list
-"""
+
 
 class MyList(list):
-    """
-    class of Mylist inherite from list
-    """
+    """Class MyList inherits from list."""
 
     def print_sorted(self):
-        """
-        print a list
-        :return:
-        """
-        print(sorted(self))
+        """Prints the list, in ascending sort."""
+
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
