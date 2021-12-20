13defc3d3a8491ce939eacbca1a6efefa0ff58fb
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-20 10:43:39 -0800

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """subclass Rectangle from class BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Instantiation, constructor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
