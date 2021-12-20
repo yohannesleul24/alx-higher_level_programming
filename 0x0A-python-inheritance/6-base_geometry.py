9c1a135b7172fc3c984156e4996468c4f162db3d
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-12-20 10:35:57 -0800

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Geometry module
+
+"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+
+    def area(self):
+        """Public instance method Raise Exception"""
+        raise Exception('area() is not implemented')
