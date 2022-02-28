981c2b9b5d3eef9bb7588ab5e471431613269f70
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2022-02-28 10:41:38 -0800

    done

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -4,14 +4,5 @@ const myObject = {
     value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
-console.log(myObject);#!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
 myObject.value = 89;
 console.log(myObject);
