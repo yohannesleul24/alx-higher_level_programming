e4ff3b1a31bc12c42c7f1bae1d3ed0bae4bcb9b5
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2022-03-17 06:47:38 -0700

    done

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash Script that takes in a URL as an argument, sends a GET request to the URL
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=hr@yohannesleul24.com&subject=I will always be here for PLD" "$1"
